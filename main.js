c0b259833c70b5242a3ced3b4229d6d0a00e34b5
Quân Tóc Đỏ <30405646+nguyenquan6196@users.noreply.github.com>
2019-07-05 10:43:39 +0700

    create initial files

---
+++ main.js
@@ -0,0 +1,21 @@
+new Vue({
+    el: '#app',
+    data: {
+        gameRunning: false,
+        gameTitle: 'Infinity War',
+        characters: [
+            {
+                name: 'Iron Man',
+                avatarUrl: './images/ironman.png',
+                role: 'player',
+                heal: 100
+            },
+            {
+                name: 'Thanos',
+                avatarUrl: './images/thanos.png',
+                role: 'enemy',
+                heal: 100
+            }
+        ]
+    }
+});
